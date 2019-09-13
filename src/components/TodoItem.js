bdb325b7780a55d6f729af0164a6de47254a216c
Thomas Ehmann <ehmannth@gmail.com>
2019-09-13 09:41:16 +0200

    initial commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <h1>todo item</h1>
+      </div>
+    )
+  }
+}
