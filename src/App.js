f23ced0b65d1110a2491fb8de491aa5888bbee30
Thomas Ehmann <ehmannth@gmail.com>
2019-09-13 18:55:54 +0200

    Changed item title

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-5">
-            <h3 className="text-capitalize text-center">todo input</h3>
+            <h3 className="text-capitalize text-center">What to do?</h3>
             <TodoInput
               item={this.state.item}
               handleChange={this.handleChange}
